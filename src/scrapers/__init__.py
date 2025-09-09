e23cee7abdf54600ff226acbc180e7006dc0df1b
Viktor Vrzhesnevskyi <github.prenatal113@passinbox.com>
2025-09-09 12:28:40 +0200

    update headers in scrapers

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -41,7 +41,7 @@ class MatchOdds(BaseModel):
 
 HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
+    # "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "en-US,en;q=0.9",
     "Cache-Control": "no-cache",
     "Pragma": "no-cache",
